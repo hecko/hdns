3c1e0c245ed84c40bb36f414f5a0db62b0829098
Marcel Hecko <maco@blava.net>
2013-04-07 12:39:06 +0100

    enable logging

--- main.pl
+++ main.pl
@@ -4,6 +4,10 @@ use strict;
 use warnings;
 use Net::DNS::Nameserver;
 use Net::DNS::Resolver;
+
+# enable autoflush to terminal
+$| = 1;
+open my $log,'>>','domain.log';
  
 sub reply_handler {
   my ($qname, $qclass, $qtype, $peerhost,$query,$conn) = @_;
@@ -11,7 +15,7 @@ sub reply_handler {
   my ($rcode, @ans, @auth, @add);
   my $action = "lookup";
 
-  print "* $qname ($qtype) from $peerhost ";
+  print $log "* $qname ($qtype) from $peerhost ";
 
   if ($qtype ne "A") {
     $action = 'nxdomain';
@@ -39,19 +43,23 @@ sub reply_handler {
       my $ret = new Net::DNS::RR("$qname 3600 $qclass $qtype $rdata");
       push @ans, $ret;
       $rcode = "NOERROR";
-      print "-> $rdata ($qtype)\n";
+      print '.';
+      print $log "-> $rdata ($qtype)\n";
     } else {
       $rcode = "NXDOMAIN";
-      print "-> A entry not found\n";
+      print 'x';
+      print $log "-> A entry not found\n";
     }
   } elsif ($action eq "forward") {
     my $ret = new Net::DNS::RR("$qname 3600 $qclass A 80.76.124.4"); 
     push @ans, $ret;
     $rcode = "NOERROR";
-    print "-> $action\n";
+    print 'ˆ';
+    print $log "-> $action\n";
   } else {
+    print 'x';
     $rcode = "NXDOMAIN";
-    print "-> $action\n"; 
+    print $log "-> $action\n"; 
   }
 
   # mark the answer as authoritive (by setting the 'aa' flag
@@ -65,3 +73,5 @@ my $ns = new Net::DNS::Nameserver(
     ) || die "couldn't create nameserver object\n";
  
 $ns->main_loop;
+
+close $log;
